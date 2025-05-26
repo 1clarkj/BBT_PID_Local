2796abb1cab79c80d7eb57bee6b8e8650b0be48f
John Douglas Clark <clark.john.douglas@gmail.com>
2025-05-26 16:31:04 +0200

    took out the printn

--- pid_control.py
+++ pid_control.py
@@ -205,7 +205,5 @@ while True:
     # PID -60-60
     current_servo_positions = (-(pid[0] - 5), -(pid[1] - 5))
     controller._servo_controller.set_degrees_bbt(current_servo_positions)
-
-    print(f"Ball position: {b_pos}, PID output: {pid}, Desired position: {target}")
     
     time.sleep(0.004)  # Small delay for control loop timing
