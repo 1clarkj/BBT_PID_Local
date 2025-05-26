6b1e35f0917893537aa9e4fc50463090cfbfe9ad
John Douglas Clark <clark.john.douglas@gmail.com>
2025-05-26 17:20:26 +0200

    step size change

--- pid_control.py
+++ pid_control.py
@@ -17,8 +17,8 @@ K_P = (0.25, 0.25)
 K_I = (0.01, 0.01)  # Introduce small integral gain for now
 K_D = (25, 25)
 
-e_filter = 5
-d_filter = 10
+e_filter = 7
+d_filter = 30
 
 desired_position = [122.5, 100.5]  # use list, so it can be updated in thread
 
